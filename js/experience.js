559b3684744091897afc6f3d633779deaf60a381
Priyanka <awatramanipriyanka1@gmail.com>
2020-08-02 13:36:47 +0530

    Initial Commit

---
+++ js/experience.js
@@ -0,0 +1,17 @@
+//   var waypoint = new Waypoint({
+//     element: document.getElementById('experience'),
+//     handler: function(direction) {
+//     //   console.log('Basic waypoint triggered');
+//       var target = document.getElementById('experience');
+//       if (direction == "down") {
+//         // target.classList.remove("navbar");
+//         target.classList.add("header_bg");
+//       }
+//       else{
+//         // target.classList.remove("fixed");
+//         // target.classList.add("navbar");
+//       }
+//     //   element.addClass("header_bg");
+
+//     }
+//   })
